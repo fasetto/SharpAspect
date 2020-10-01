5ac7274b216409c99e95961423b96259fe2783b2
SERKAN <serkanbircan21@yandex.com>
2020-10-01 15:31:23 +0300

    removed comment lines I used as notes

--- samples/SharpAspect.Sample/Program.cs
+++ samples/SharpAspect.Sample/Program.cs
@@ -2,9 +2,6 @@
 using Castle.DynamicProxy;
 using Microsoft.Extensions.DependencyInjection;
 
-// https://blog.zhaytam.com/2020/08/18/aspnetcore-dynamic-proxies-for-aop/
-// https://github.com/castleproject/Core/blob/master/docs/dynamicproxy-leaking-this.md
-
 namespace SharpAspect.Sample
 {
     class Program
